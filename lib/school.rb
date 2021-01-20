7e8106ae4a3a43737d8d2a80c50f4eed029f3905
prodigysurge <boatinglife6@yahoo.com>
2021-01-20 16:21:05 -0500

    commit tree 5e7553468e41d633eb95739f0210e0342b9f253a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+class School
+
+    attr_accessor :roster, :student_name, :school_name, :grade
+
+    
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+        @student_name
+    end
+
+    def add_student(student_name, grade)
+        if @roster[grade] 
+            @roster[grade] << student_name
+        else
+            @roster[grade] =[]
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+
+        @roster[grade].sort
+
+    end
+
+    def sort
+        @roster.each do |grade, student|
+            student.sort!
+        end
+    end
+    
+
+ 
+      
+end
+
